910e3accaa4bae9d197c8f25c970e33f7ac52188
michael-ploetz <mjploetz@icloud.com>
2022-02-07 19:04:31 +0100

    prerelease version

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const sslRedirect = require('heroku-ssl-redirect').default;
-const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -15,8 +14,6 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(favicon(path.join(__dirname, 'public', 'assets', 'icon.png')));
 
 app.use('/api/skoodles', require('./routes/api/skoodles'));
 
